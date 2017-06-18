8dea8ba1fbffb53b73614b3e3b5b2373de61632c
Delcio Torres <delciotorres@gmail.com>
2017-06-18 00:17:02 -0300

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyintralinks',
-    version='0.1.0',
+    version='0.1.1',
     description='A python client for intralinks api.',
     long_description='Simplifies the use of Intralinks API.',
     url='https://github.com/delciotorres/pyintralinks',
